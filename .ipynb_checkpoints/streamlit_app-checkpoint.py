eddb9ae3a4c3ef6c48014f840f1b08a8c732c05c
Andrew Scannell <andy.scannell@hotmail.co.uk>
2024-03-12 15:11:27 +0000

    update app

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -7,7 +7,7 @@ import numpy as np
 st.header('Let\'s hope this works!')
 st.write('After numerous errors lets hope we get none')
 
-map_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
+map_data = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [50, -1], columns=['lat', 'lon'])
 
 st.map(map_data)
 
