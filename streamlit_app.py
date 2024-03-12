986bbd42684a25526dfa2f0435fca89f7d0197cb
Andrew Scannell <andy.scannell@hotmail.co.uk>
2024-03-12 16:31:58 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,15 @@ import seaborn as sns
 import numpy as np
 
 
-st.header('Exoplanets')
-st.write('See data from every discovered exoplanet')
+st.header('All confirmed Exoplanet discoveries')
+st.write('Here you can see the data from every discovered exoplanet')
 st.write('Do you want to see this data?')
 if st.checkbox('Yes'):
     exo = pd.read_csv('PS_2024.03.11_08.53.21.csv',skiprows=96)
     exo.drop_duplicates(subset='pl_name', keep='last', inplace=True)
     exo.reset_index(drop=True, inplace=True)
     exo
-
-    
-if st.checkbox('No'):
+else st.checkbox('No'):
     st.write('Then why are you using this app?????')
 
 
