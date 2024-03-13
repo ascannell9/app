6b4a1462198d6121c390e0d813167a015e64bccb
Andrew Scannell <andy.scannell@hotmail.co.uk>
2024-03-13 20:04:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.header('All confirmed Exoplanet discoveries')
 st.write('Here you can see the data from every discovered exoplanet')
 st.write('Do you want to see this data?')
 
-select = st.selectbox('Answer', ['Yes','No'])
+select = st.selectbox('Answer', ['Please select','Yes','No'])
     
 if select=='Yes':
     exo = pd.read_csv('PS_2024.03.11_08.53.21.csv',skiprows=96)
@@ -21,7 +21,7 @@ if select=='Yes':
     x = np.random.randint(5594)
     st.write(f"You have been selected to go to planet {exo.loc[x]['pl_name']}")
     st.write(f"Here are some details about {exo.loc[x]['pl_name']:}")
-    st.write(f"{exo.loc[x]}")
+    st.table(f"{exo.loc[x]}")
 else:   
     st.write('Then why are you using this app?????')
     
