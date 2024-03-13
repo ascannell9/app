e1b4dc439e8b60050944dd192c29ffbae4a5f22a
Andrew Scannell <andy.scannell@hotmail.co.uk>
2024-03-13 20:00:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,18 +8,21 @@ import numpy as np
 st.header('All confirmed Exoplanet discoveries')
 st.write('Here you can see the data from every discovered exoplanet')
 st.write('Do you want to see this data?')
-if st.checkbox('Yes', on_change='Yes'):
+
+select = st.selectbox('Answer', ['Yes','No'])
+    
+if select=='Yes':
     exo = pd.read_csv('PS_2024.03.11_08.53.21.csv',skiprows=96)
     exo.drop_duplicates(subset='pl_name', keep='last', inplace=True)
     exo.reset_index(drop=True, inplace=True)
     st.dataframe(exo)
-    st.write('You can assign yourself a new planet to move to with this random number generator')
-    st.button('Random number generator')
+    st.write('You can assign yourself a new planet to move to by clicking the button below')
+    st.button('Random planet generator')
     x = np.random.randint(5594)
     st.write(f"You have been selected to go to planet {exo.loc[x]['pl_name']}")
     st.write(f"Here are some details about {exo.loc[x]['pl_name']:}")
     st.write(f"{exo.loc[x]}")
-if st.checkbox('No'):    
+else:   
     st.write('Then why are you using this app?????')
     
 
